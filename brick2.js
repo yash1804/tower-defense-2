b8f4f42050d4e56a038105316bbb862b3031a291
yash1804 <70275059+yash1804@users.noreply.github.com>
2020-12-19 18:21:19 +0530

    Add files via upload

---
+++ brick2.js
@@ -0,0 +1,33 @@
+class Brick2 {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: false
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display()
+  { console.log(this.body.speed);
+     if(this.body.speed <5){
+      var angle = this.body.angle;
+      var pos= this.body.position;
+      push();
+      translate(pos.x, pos.y);
+     rotate(angle);
+     rectMode(CENTER);
+     fill("blue");
+     rect(0,0,this.width, this.height);
+     this.visiblity = 255;
+      pop();
+     }
+     else
+     { World.remove(world, this.body);
+       push();       
+       this.visibility = this.visibility -1;
+       pop();
+     }
+  }
+  }
